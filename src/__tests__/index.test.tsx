105b38ac2d7a2f9410f67c218e25e08b8c643190
Wei Zhu <yesmeck@gmail.com>
2019-05-03 22:19:44 +0900

    Add more tests

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,15 +1,31 @@
-import * as React from 'react';
-import TestRenderer from 'react-test-renderer';
-import { createContextIO } from '..';
+import * as React from "react";
+import TestRenderer from "react-test-renderer";
+import { createContextIO } from "..";
 
-describe('createContextIO', () => {
-  it('works', () => {
+describe("createContextIO", () => {
+  it("works", () => {
     const Store = createContextIO(1);
     TestRenderer.create(
       <Store.Provider>
         <div />
-      </Store.Provider>,
+      </Store.Provider>
     );
     expect(Store.read()).toBe(1);
+  });
+
+  it("useContext", () => {
+    const Store = createContextIO(1);
+
+    const App = () => {
+      const count = React.useContext(Store);
+
+      return <div>{count}</div>;
+    };
+    const testRenderer = TestRenderer.create(<App />);
+    expect(testRenderer.toJSON()).toMatchInlineSnapshot(`
+      <div>
+        1
+      </div>
+    `);
   });
 });
